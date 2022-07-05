9548752ebe13b3eee5ee5f77054bbf047e75e5d7
João Maia <102671695+JoaoMaiaBGM@users.noreply.github.com>
2022-07-05 15:00:44 -0300

    feat: requisição delete.

---
+++ src/js/deletar.js
@@ -0,0 +1,39 @@
+import { Api } from "./Api.js";
+
+const clientes = await Api.listarClientes();
+const listaClientesNome = document.getElementById('buscarCliente');
+
+const nomesClientes = (cliente) => {
+  
+  cliente.forEach(({nome, id}) => {
+  const option = document.createElement('option');
+  option.value = id;
+
+  option.innerText = nome;
+
+    listaClientesNome.append(option);
+  });
+
+}
+nomesClientes(clientes)
+
+const btnDeletarCliente = document.getElementById('deletarCliente');
+btnDeletarCliente.addEventListener('click', handleEventDelete)
+
+async function handleEventDelete(event) {
+  const listaBuscarCliente = document.getElementById('buscarCliente');
+
+  for(let i = 0; i < clientes.length; i++) {
+    event.preventDefault();
+    
+    if(clientes[i].id == listaBuscarCliente.value) {
+
+      const pessoa = clientes[i]
+      console.log(clientes[i].id)
+      console.log(listaBuscarCliente.value)
+
+      Api.deletarCliente(pessoa.id);
+
+    }      
+  }
+}
