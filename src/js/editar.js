834dab98b6388bf2e86a0049e56cfc6c713a3a7e
João Maia <102671695+JoaoMaiaBGM@users.noreply.github.com>
2022-07-05 08:24:15 -0300

    feat: mostrando todos os dados do cliente no formulário editar.

--- src/js/editar.js
+++ src/js/editar.js
@@ -16,3 +16,52 @@ const nomesClientes = (cliente) => {
 
 }
 nomesClientes(clientes)
+
+
+const btnBuscarCliente = document.getElementById('btnBuscarCliente');
+btnBuscarCliente.addEventListener('click', handleEvent);
+
+function handleEvent() {
+  event.preventDefault();
+  const listaBuscarCliente = document.getElementById('buscarCliente');
+
+  for(let i = 0; i < clientes.length; i++) {
+    if(clientes[i].id == listaBuscarCliente.value) {
+      const pessoa = clientes[i];
+
+      const inputNome = document.getElementById('nome');
+      inputNome.value = pessoa.nome;
+
+      const inputDataNasc = document.getElementById('data_nasc');
+      inputDataNasc.value = pessoa.data_nasc;
+
+      const optionSexo = document.getElementById('sexo_masculino');
+      optionSexo.innerText = pessoa.sexo;
+
+      const inputEmail = document.getElementById('email');
+      inputEmail.value = pessoa.email;
+
+      const inputCep = document.getElementById('cep');
+      inputCep.value = pessoa.endereco.cep;
+
+      const inputRua = document.getElementById('rua');
+      inputRua.value = pessoa.endereco.rua;
+
+      const inputNumero = document.getElementById('numero');
+      inputNumero.value = pessoa.endereco.numero;
+
+      const inputBairro = document.getElementById('bairro');
+      inputBairro.value = pessoa.endereco.bairro;
+
+      const inputCidade = document.getElementById('cidade');
+      inputCidade.value = pessoa.endereco.cidade;
+
+      const inputEstado = document.getElementById('estado');
+      inputEstado.value = pessoa.endereco.estado;
+
+      const inputTelefoneFixo = document.getElementById('telefone_fixo');
+      inputTelefoneFixo.value = pessoa.telefone_fixo;
+
+    }
+  }
+}
